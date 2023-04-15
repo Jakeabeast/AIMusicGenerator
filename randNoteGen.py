0cba9c6ae99d2f479f0e064b69352f40c74a603b
Jake <63001043+Jakeabeast@users.noreply.github.com>
2023-04-16 01:19:12 +1000

    Musical Note Generator

---
+++ randNoteGen.py
@@ -0,0 +1,9 @@
+import random
+
+musicNotes = ["a", "b", "c", "d", "e", "f", "g"]
+
+print("{ ", end=" ")
+for i in range(8):
+    print(random.choice(musicNotes) + '\' ', end=" ")
+
+print("} ", end=" ")
