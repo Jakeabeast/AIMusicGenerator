b6b114a6c6e78b595f8daf88ee31abe2a547a411
Jake <63001043+Jakeabeast@users.noreply.github.com>
2023-04-17 03:56:29 +1000

    note durations added (whole, half, quarter) + notes beam over bar

--- randNoteGen.py
+++ randNoteGen.py
@@ -1,10 +1,62 @@
 import random
 
-musicNotes = ["a", "b", "c", "d", "e", "f", "g"]
+class MusicCreator:
+    def __init__ (self, bars):
+        self.BARS = bars
+        self.timeSignature = [4,4] #in common time
+        self.lengthByTimeSig = BARS * self.timeSignature[1]
+        self.musicNotes = ["a", "b", "c", "d", "e", "f", "g"]
 
-print("{ ", end=" ")
-for i in range(8):
-    print(random.choice(musicNotes) + '\' ', end=" ")
+    def random_note(self):
+        return random.choice(self.musicNotes)
+    
+    def convert_for_lilypond(self, note, noteDuration):
+        if (noteDuration == 3): #convert to dotted note
+            string = note + "\'2."
+        else:
+            durationNotation = int(noteDuration ** -1 * self.timeSignature[1])
+            string = note + "\'" + str(durationNotation)
+            
+        return string
+            
 
-print("} ", end=" ") 
+    def print_musical_list(self):
+        
+        remainingSong = self.lengthByTimeSig 
+        remainingBar = self.timeSignature[1]
+
+        print("{ ", end=" ")
+
+        while (remainingSong > 0):
+            note = self.random_note()
+            noteLength = random.choices([4,2,1], weights=(14,28,58)) # 1 = whole, 2 = half, 4 = quarter
+            noteLength = noteLength[0]
+
+            if (noteLength > remainingSong): #does it fit at the end of bar
+                continue
+            elif (noteLength > remainingBar): #note needs to beam over bar (spagetti for now)
+                print(self.convert_for_lilypond(note, remainingBar) + "~ " 
+                      + self.convert_for_lilypond(note, noteLength - remainingBar), end="  ")
+                remainingBar = self.timeSignature[1] - (noteLength - remainingBar)
+            else:
+                
+                print(self.convert_for_lilypond(note, noteLength), end="  ")          
+                remainingBar -= noteLength
+                if (remainingBar <= 0):
+                    remainingBar = self.timeSignature[1]
+
+            remainingSong-=noteLength
+        #print(self.durationList) #to view list in terminal
+            
+        print("} ", end=" ") 
+
+
+if __name__ == "__main__":
+    BARS = 8 #input number of bars
+
+    test = MusicCreator(BARS)
+    test.print_musical_list()
+
+
+
 
